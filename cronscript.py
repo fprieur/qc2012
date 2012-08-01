a0c7939f0f2b8e3170ff6b56c3aa033e6f8622f0
Fred Prieur <g-fallevato@Fred-Prieur-MacBook-Pro.local>
2012-08-01 17:25:29 -0400

    ajout du script pour le cron

---
+++ cronscript.py
@@ -0,0 +1,16 @@
+import urllib
+import json
+
+def tweetsqc2012():
+	search = urllib.urlopen("http://search.twitter.com/search.json?q=qc2012&rpp=50&include_entities=true&result_type=mixed")
+ 	dict = json.loads(search.read())
+ 	objects_list = []
+ 	for result in dict["results"]:
+  		objects_list.append(result)
+
+  	j = json.dumps(objects_list)
+	f = open("qc2012.json",'w')
+	f.write(j)
+	f.close()
+
+tweetsqc2012()
