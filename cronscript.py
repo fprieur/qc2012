54b42dade61377d4c327bf6c340299b05483535c
Fred Prieur <g-fallevato@Fred-Prieur-MacBook-Pro.local>
2012-08-03 07:54:52 -0400

    modif du hash apres tests qui ont marchés

--- cronscript.py
+++ cronscript.py
@@ -3,7 +3,7 @@ import urllib
 import json
 
 def tweetsqc2012():
-	search = urllib.urlopen("http://search.twitter.com/search.json?q=bieber&rpp=50&include_entities=true&result_type=mixed")
+	search = urllib.urlopen("http://search.twitter.com/search.json?q=qc2012&rpp=50&include_entities=true&result_type=mixed")
  	dict = json.loads(search.read())
  	objects_list = []
  	for result in dict["results"]:
