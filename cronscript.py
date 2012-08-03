487978c036d9d458d254cad840d24404c4034914
Fred Prieur <g-fallevato@Fred-Prieur-MacBook-Pro.local>
2012-08-03 09:48:16 -0400

    ajout d'un toolbox, et ajout de la date de modification

--- cronscript.py
+++ cronscript.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 import urllib
 import json
+import toolbox
+import datetime
 
 def tweetsqc2012():
 	search = urllib.urlopen("http://search.twitter.com/search.json?q=qc2012&rpp=50&include_entities=true&result_type=mixed")
  	dict = json.loads(search.read())
  	objects_list = []
+ 	chemin = toolbox.determinerEnvironnement()
  	for result in dict["results"]:
   		objects_list.append(result)
 
   	j = json.dumps(objects_list)
-	f = open("/var/www/qc2012/qc2012.json",'w')
+	f = open(chemin + "qc2012.json",'w')
 	f.write(j)
 	f.close()
+
+	now = datetime.datetime.now()
+	f2 = open(chemin +  "updateDate.txt",'w')
+	f2.write("Dernière mise à jour le : " + str(now.day) + " " +  toolbox.moisEnFrancais(now.month) + " " + str(now.year) + " " + "à " + str(now.hour) + ":" + str(now.minute))
+	f2.close
+
 
 tweetsqc2012()
---
+++ toolbox.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import datetime
+import os
+
+def moisEnFrancais(intMois):
+	if (intMois == 1):
+		return 'Janvier'
+	elif (intMois == 2):
+		return 'Février'
+	elif (intMois == 3):
+		return 'Mars'
+	elif (intMois == 4):
+		return 'Avril'
+	elif (intMois == 5):
+		return 'Mai'
+	elif (intMois == 6):
+		return 'Juin'
+	elif (intMois == 7):
+		return 'Juillet'
+	elif (intMois == 8):
+		return 'Août'
+	elif (intMois == 9):
+		return 'Septembre'
+	elif (intMois == 10):
+		return 'Octobre'
+	elif (intMois == 11):
+		return 'Novembre'
+	elif (intMois == 12):
+		return 'Décembre'
+
+def determinerEnvironnement():
+	path = ""
+	if(os.getlogin == "ubuntu"):
+		path = "/var/www/qc2012/"
+	return path
